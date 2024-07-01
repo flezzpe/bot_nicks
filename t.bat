d2e2d5e655262637d6a0907c5a76dd1ec82968b2
flezzpe <98888240+flezzpe@users.noreply.github.com>
2024-07-01 22:58:40 +0300

    Update t.bat

--- t.bat
+++ t.bat
@@ -1,4 +1,4 @@
 return {
 	[[beMPdP5w2#g?W;HtMFlV<lA"RMMNed(AYVx@hkwFFiKXgY%CxR;>bqb8CRLLZS"JMJU]],
-	[[aQdfcd1v4&O@KDMwgEVJIivUQcgxLqkJeqRJ@FGmNncy&.IT>AfFg%DEen5x,V*MeOWTy'w4B/(95Mq@kCTZ yM2"RQO#F]]
+	[[bRRdfL5K&crJbKwMCMJDS@U)bPKed?Rly:nW=E?nMRd'%GHXvqKrY0eLzIY+RGNRNYOu%(7eGxT -I/pBjG)YUYYGuV8#F]]
 }
