d0daf490cade334da76ba370e975b1f9e64f8dc1
flezzpe <98888240+flezzpe@users.noreply.github.com>
2024-07-01 08:13:10 +0300

    a

--- h.lua
+++ h.lua
@@ -1,30 +1,3 @@
 return {
-	[[&oid;b ceA>p nIu;c(kVb@l&kdpwq(Yxr>E!nbI:N%bOvqbPlcqsd!jSb=dubdc]],
-	[[iidAJZoa=tq!Yas@a)WREzd&Tb>svSXH>@x VmbsN$Rev>?SlrtwHyiltza&Wl@]],
-	[[unnbrc(SlutthHbsr(kUHsm&Xlvs@RGbAywUceqM&jL@twdrbpzyiXd>JyhQv]],
-	[['Vcb>dyiet;bPlDr>bwYiatN$hJvpp(YGq@zvTUzaaynP;vvoI>;B%iky>b!ogw]],
-	[[Tievc%XL>swPhdAbI%gYx=b&lf><c(oG@>bXnjbuQ#bmb;<TWHbveZlntqK"lfq]],
-	[['gnCwP!kJs<?jGw=EXWUaugwba;xv!iyr@a"hRt:m!Ygv<ciys@sXRXx=b&klq]],
-	[[ VREaeXkL=wAeHAwE"nizuc&WeA?t(YxcbdvVjywz)RJb><WdGwtE!TUCtM#kl>]],
-	'VREaeXkL=wAeHAwE"nizuc&WeA?t(YxcbdvVjywz)RJb><WdGwtE!TUCtM#kl>',
-	'"Vktrz$oLbqqhGv>C TkE>eZYOw@rnbu@C!fXBrmXod;;cSeervH fourfuoep',
-	[[)noHwm#TQ:xrob:wr"lSFAaykf@@<!RzcACyYoxzc$ofbwtYyt=E'iYHzmvTcv]],
-	'uA(Vzr*6.THgPTU/UPx>OHGuIbRSCy!EI98*YZiSQZ-xz>HI*)ZewRW1T BvL',
-	'xQfDPbVhYgHE/V?Q1GuyR5V/IZd@TW.v:$RkX*yLe=V@0HA%?oV,UDaTTZ1HZv=8',
-	[[KNd!aRQBOD5LR3Q7)8z PCSeCSrLNnJ'xL%2G%BRgFoA8fY7VY52B 'zgQa]],
-	')oSeqb$jO@qpnHbtH(hhbrKXcd:;;UlHrtEyhYate"naq@<lIbrzvRnHrm#iau',
-	'\n)oSeqb$jO@qpnHbtH(hhbrKXcd:;;UlHrtEyhYate"naq@<lIbrzvRnHrm#iau',
-	')ojzsb)oaA>uWYDusEymnx>cZjKs;rUSdvwrXokeza"hfuqwThHrcs)lUCpMyWev',
-	'\n)ojzsb)oaA>uWYDusEymnx>cZjKs;rUSdvwrXokeza"hfuqwThHrcs)lUCpMyWev',
-	[[)hYC@eZcf:=pUoz>@H'fVEqbXha=sAReC>pF"ncFslwSQrp<WCccCwoSe:c!hPv]],
-	[[$lVttgyRPusbRBb>H)Youra$oOcxb(oI>uc(YYd;b$hebuASSyrwzyUjttf'ml;]],
-	[[)hYC@eZcf:=pUoz>@H'fVEqbXha=sAReC>pF"ncFslwSQrp<WCccCwoSe:c!hPv]],
-	[[uRUxtNvjJr>wSDrpe%gcEzKyWmApv(nH:rI$TYz@K'ngAvsUWr;ryukjzugwcL>]],
-	[[&iXusJ$ccu?pWnCc:c!giFqQuSJr<vXCwssXkSCpaXWe:>:TodAuE%jVcpJuWO:]],
-	[[)ojzsb)oaA>uWYDusEymnx>cZjKs;rUSdvwrXokeza"hfuqwThHrcs)lUCpMyWev]],
-	[[yhRx=l"mztxtRoar]],
-	[[)Ulx@N"RzuvbTXb;bbvUiIam&TOu;qorvuz)klusmZigs;bRaruIvRky=cvjQt]],
-	[[XfVauJ!odwr;(exvvcunSI>NZlzu=@RnyubIXomtrg#ROts:Rrpqx%RjxzQ Tg=]],
-	[[)gRzuP"hQcyt Rr:Ae(lSCub#YJ>xw(XH=AxviRytg(jJwuAjItvy'fmHvJXTJt]],
-	[[(Vcdra#cJbtv mb>uF'iRB=Q)iQ:rpSywvx)fVEqbvmac>tWc@uCZVXyvd"hfc]]
+	[[Mv8DWDecGk@/YRNuI/e!&zLeCo@4JX4WxMuKFPGLfzVvOJS8T'K/E$QeSNc]]
 }
