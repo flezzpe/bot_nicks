5c3e13d0e237593f90ad241a59f891f2ec9e4aee
flezzpe <98888240+flezzpe@users.noreply.github.com>
2024-07-02 20:45:11 +0700

    Update h.lua

--- h.lua
+++ h.lua
@@ -7,5 +7,6 @@ return {
 	[[7/H'aOJAi=2dU6u6u.xQ!xgobl@8fS9XM$3eAPdIkENXWIxK(4GAbgG]],
 	[[L5eVBRdHY<8fn5'P5$6d$'@ceeYA.cmLPP8(KBTGOOBWA8QY8W%8v/zUQAKNI]],
 	[[1(8IVPzOcxS@6dU3VY8ZOz%FzzeX;HenNVZ1u4GTDbgGirHQL8!&N(5?%Fgnc]],
-	[[0v.HQePzBn:OJjMWX9#LyWQcdLbip8PL4#KI>QGdJDW<8fX9u0v9x!'xJNx]]
+	[[0v.HQePzBn:OJjMWX9#LyWQcdLbip8PL4#KI>QGdJDW<8fX9u0v9x!'xJNx]],
+	[[3u2EWQISOcUvNKS2!'M(NcPxSocTwKbTITvJv/H$cLfHXrLan7x8y.y%TCbgI]]
 }
