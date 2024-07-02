aa601619cbd40b89cf678680b8025b6f323e0b81
flezzpe <98888240+flezzpe@users.noreply.github.com>
2024-07-02 20:39:18 +0700

    Update t.bat

--- t.bat
+++ t.bat
@@ -1,4 +1,12 @@
 return {
 	[[beMPdP5w2#g?W;HtMFlV<lA"RMMNed(AYVx@hkwFFiKXgY%CxR;>bqb8CRLLZS"JMJU]],
-	[[bRRdfL5K&crJbKwMCMJDS@U)bPKed?Rly:nW=E?nMRd'%GHXvqKrY0eLzIY+RGNRNYOu%(7eGxT -I/pBjG)YUYYGuV8#F]]
+	[[bRRdfL5K&crJbKwMCMJDS@U)bPKed?Rly:nW=E?nMRd'%GHXvqKrY0eLzIY+RGNRNYOu%(7eGxT -I/pBjG)YUYYGuV8#F]],
+	[[efaOPaM"I'e=Kb4<PDYV=iEUWfSaGc<olGuXmtExhKRdy&+ynDqNAc#0CPdNDQ"JQJU]],
+	[[NeNeMdJ(NSsJeOvQHYzInAScKaEN(tLJ=:LW<EIUQhdv/xSwpQwQ0eNNN#/ "JMJU]],
+	[[dMSPNS0Z9S;Lb8<NGVQyQpTPMgId%Bkhe;YSuDFUclzx#ECnrsKuQ,CSg7'/"JRJU]],
+	[[LSRLMRJ'Ke:JzK@LFSg=kqPfSabd$qRSI:hT?eDSUTe&v8yP?sL<b%+IQg.y-"JLJU]],
+	[[cSPcQc39$M;Px8;UAXPEmD#(bMbIetljeBUXwBDmKSN%$,CYA>awQY8xgL.&Z"JMJU]],
+	[[RQMMeeK'2vO?NzK=zHUfxiEaPbHcvrohy<Lk@GGmPnf(xGeYD>Kpg%*ASb9Z4S"JNJU]],
+	[[dMSPNS0Z9S;Lb8<NGVQyQpTPMgId%Bkhe;YSuDFUclzx#ECnrsKuQ,CSg7'/"JRJU]],
+	[[SeSLLdN(5&b=gH4waITOajwUUedayL$;MVEqnKu>IhKhK#$1Cn@?MAYY.zOnK'ZV"JSJU]]
 }
