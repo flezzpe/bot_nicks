c6de06be6f83625a07a1c5144a7220a377baaf00
flezzpe <98888240+flezzpe@users.noreply.github.com>
2024-07-10 23:55:38 +0700

    Update t.bat

--- t.bat
+++ t.bat
@@ -57,5 +57,6 @@ return {
 	[[ScPffbN$I"SqVHOvKIWP<YrUUSNMHd#BShb@oKBe?Nchd'&E<U=wK=c#0azc5$1R-OObmmvy MB/uWPBO5@bR-!&(o+%! N# F]],
 	[[RaRLKP1NxL:J:2?OdiP<P;P)JQJCh'viRGsUm:dHOdhO)#CDPq:awgx9zgL3"0V(JNJU]],
 	[[cfONRe3XI#Q;NHK:QBlPIP:ReQGz#tRheuWK>HIXNkP(-=l:;Kvc)0HJb3$GV(JOJU]],
-	[[ROLPLe1yNXOuaH3:aBRaEjrW!MNUDOvXYCsYTtdCRzhd"#8dn?Aarzy.aen/'FS"JRJU]]
+	[[ROLPLe1yNXOuaH3:aBRaEjrW!MNUDOvXYCsYTtdCRzhd"#8dn?Aarzy.aen/'FS"JRJU]],
+	[[BLAROD8z3yJUPBM>GGLPHJ> rEKBCe&ANKB;LRCyyUFY0e&.IQP@EAJ'0yDokzz(JMJU]]
 }
