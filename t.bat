631da497f6d52fb0ffcb1d46ea8582a86764e3d5
flezzpe <98888240+flezzpe@users.noreply.github.com>
2024-08-12 14:51:26 +0700

    Update t.bat

--- t.bat
+++ t.bat
@@ -122,5 +122,5 @@ return {
 	[[MOMOPP6'5)L:P:M=JCVNCT?NMNHh);VUCAVWADIUKUd#*CP:<JAO/EQz5"+*PMORS)''-y2(/3.?GV9'ZW1"48"G ,'v>L?2(8$&H*%IGKWU< !"'BVG]],
 	[[PNMSMQ3$8"Q<P:M>JGSLFT?!KMTHz$CVYC@SK<FISNUa%#/CW?>J>O$*FQe5". *PPNRX$'9b,( .37?IS*'uW*"78)>1"Y?Q?2%5'&!B/%GBVRXA"%BVG]],
 	[[MNMJPL6(5)Q>MBH>OCSMFW?!KMUHg)=SSC;SU<?DYKRd#-FV::O:O&/CLn8%+"*SPKWY$'7b1(%.32:EV0)!yR.'25$I,'Y?Q:2#5%#B/!%EBMRW<$'BVG]],
-	[[3%8EGSeAX@5NR6 u3$8GBNeHX<5NW6 Q3'8E GKzFX;5MR9 u6&5C'BOzBX@8PW3 x6)5CBQeAX;8OW6v6&8H'BQeFXE5NR1 u6%8HBQzDU:5MW4 u6(]]
+	[[PLMQMN68L?MDH@JFVPCX?!OMMHz)=VTCCVW<EDYKWa)#.FU:?JAO#*ELn5". *OMKWT$$'7b.(!+3/:FV."!xR.'.8)H',"vCLA2#8$&C*(CGJRX<!#'DVG]]
 }
